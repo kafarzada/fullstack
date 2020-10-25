1dbe7b6f72a6654bf3e1ad0bc6b7a8087d39e992
Kafarzada Vurgun <kafarzada@gmail.com>
2020-10-25 18:57:33 +0700

    добавлен body parser cors morgan

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryroutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
 
+app.use(morgan('dev'))
+app.use(cors())
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
